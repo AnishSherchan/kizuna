924a49f912a90694e50cb7d8f870e686f7db1934
AnishSherchan <sherchananish11@gmail.com>
2025-06-18 22:02:31 +0545

    Steps Completed

--- components/molecule/Cards/StepsCard.tsx
+++ components/molecule/Cards/StepsCard.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-const StepsCard = () => {
-  return <div>StepsCard</div>;
+import Text from "@/components/atom/Text";
+
+const StepsCard = ({
+  title = "Reach out",
+  description = "Contact us via phone, email or our website",
+  Icon,
+}: {
+  title: string;
+  description: string;
+  Icon: React.FC<React.SVGProps<SVGSVGElement>>;
+}) => {
+  return (
+    <div className=" max-w-[266px] px-[30px] flex flex-col gap-[40px]">
+      <Icon />
+      {/* Texts */}
+      <div>
+        <h2 className=" text-[24px] font-normal text-center">{title}</h2>
+        <Text variant="card_text" className=" text-[#747474] text-center">
+          {description}
+        </Text>
+      </div>
+    </div>
+  );
 };
 
 export default StepsCard;
--- components/organism/Home/Steps.tsx
+++ components/organism/Home/Steps.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { useTranslations } from "next-intl";
+import { BsChevronCompactRight } from "react-icons/bs";
 
+import {
+  ReachOutSvg,
+  ConsultationSvg,
+  SupportSvg,
+} from "@/public/assets/icons";
 import ContentWrapper from "@/components/Layout/ContentWrapper";
 import StepsCard from "@/components/molecule/Cards/StepsCard";
 
@@ -13,7 +19,25 @@ const Steps = () => {
       subHeading={t("spanTitle")}
       headingSpan={t("stepsDescription")}
     >
-      <StepsCard />
+      <main className=" w-full flex flex-col gap-6 lg:gap-0 lg:flex-row justify-evenly items-center flex-wrap">
+        <StepsCard
+          title={t("steps.step1.title")}
+          description={t("steps.step1.description")}
+          Icon={ReachOutSvg}
+        />
+        <BsChevronCompactRight className=" rotate-90 lg:rotate-0 text-[#747474] h-[45px] w-[45px]" />
+        <StepsCard
+          Icon={ConsultationSvg}
+          title={t("steps.step2.title")}
+          description={t("steps.step2.description")}
+        />
+        <BsChevronCompactRight className=" rotate-90 lg:rotate-0 text-[#747474] h-[45px] w-[45px]" />
+        <StepsCard
+          title={t("steps.step3.title")}
+          Icon={SupportSvg}
+          description={t("steps.step3.description")}
+        />
+      </main>
     </ContentWrapper>
   );
 };
