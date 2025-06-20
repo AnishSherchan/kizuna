88f7e41016b8d6cdde5075e4becac39b9ce483e6
AnishSherchan <sherchananish11@gmail.com>
2025-06-21 03:42:26 +0545

    Form Completed

---
+++ app/services/employment-support/job-form/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+import React, { useRef } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+import PageBanner from "@/components/molecule/PageBanner";
+import MaxWidthWrapper from "@/components/Layout/MaxWithWrapper";
+import FormWrapper from "@/components/Layout/FormWrapper";
+import { useTranslations } from "next-intl";
+import JobAppForm from "@/components/molecule/Forms/JobAppForm";
+import CVPreview from "@/components/organism/EmploymentPageContent/CvPreview";
+
+const JobForm = () => {
+  const t = useTranslations("EmploymentSupportPage");
+  const cvRef = useRef(null);
+  const jobSchema = useFormik({
+    initialValues: {
+      name: "",
+      mail: "",
+      gender: "",
+      phone: "",
+      address: "",
+      skills: "",
+      workExperience: "",
+      EducationLevel: "",
+      JpLanguageLevel: "",
+      EnglishLanguageTest: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Name is Required."),
+      mail: Yup.string()
+        .email("Invalid email format.")
+        .required("Email is required."),
+      gender: Yup.string().required("Please Select Gender."),
+      phone: Yup.string()
+        .matches(/^[0-9]{10}$/, "Number must be 10 digits")
+        .required("Phone number is required"),
+      address: Yup.string().required("Address is required."),
+      skills: Yup.string(),
+      workExperience: Yup.string(),
+      EducationLevel: Yup.string().required(
+        "Please Select your passed education level."
+      ),
+      JpLanguageLevel: Yup.string(),
+      EnglishLanguageTest: Yup.string(),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      console.log(values);
+      resetForm();
+    },
+  });
+
+  return (
+    <main>
+      <PageBanner
+        title={t("jobApplicationForm")}
+        bgUrl="/assets/images/BackGround/Meeting.webp"
+        className="bg-center"
+      />
+      <MaxWidthWrapper isPageContent className=" flex flex-col gap-[30px]">
+        <FormWrapper>
+          {/* Form */}
+          <JobAppForm jobSchema={jobSchema} />
+          {/* PDF */}
+          <CVPreview formData={jobSchema.values} ref={cvRef} />
+        </FormWrapper>
+      </MaxWidthWrapper>
+    </main>
+  );
+};
+
+export default JobForm;
---
+++ components/Layout/FormWrapper/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BiArrowBack } from "react-icons/bi";
+import { useTranslations } from "next-intl";
+
+const FormWrapper = ({ children }: { children: React.ReactNode }) => {
+  const t = useTranslations("ServicesPage");
+  return (
+    <div className=" w-full flex flex-col py-[40px] gap-[30px]">
+      <div className=" flex w-fit flex-row gap-[5px] text-[16px] font-medium text-[#7B7B7B] cursor-pointer items-center">
+        <BiArrowBack />
+        <p>{t("bannerTitle")}</p>
+      </div>
+      <div className=" flex justify-between gap-[30px] flex-col xl:flex-row w-full">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default FormWrapper;
