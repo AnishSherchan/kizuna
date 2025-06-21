ec8d5881b5acab834b77b989acf4ef87dc5552a8
Abhayaghmr11 <abhaya2057@outlook.com>
2025-06-21 18:08:38 +0100

    Car application with translation done

--- components/Layout/FormWrapper/index.tsx
+++ components/Layout/FormWrapper/index.tsx
@@ -6,14 +6,14 @@ import Link from "next/link";
 const FormWrapper = ({ children }: { children: React.ReactNode }) => {
   const t = useTranslations("ServicesPage");
   return (
-    <div className=" w-full flex flex-col py-[40px] gap-[30px]">
+    <div className="w-full flex flex-col py-[40px] gap-[30px]">
       <Link href={"/services"} className=" w-min">
         <div className=" flex w-fit flex-row gap-[5px] text-[16px] font-medium text-[#7B7B7B] cursor-pointer items-center">
           <BiArrowBack />
           <p>{t("bannerTitle")}</p>
         </div>
       </Link>
-      <div className=" flex justify-between gap-[30px] flex-col xl:flex-row w-full">
+      <div className="flex justify-between gap-[30px] flex-col xl:flex-row w-full">
         {children}
       </div>
     </div>
